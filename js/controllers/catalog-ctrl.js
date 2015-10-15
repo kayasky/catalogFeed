02a1320b308e6a352a8826760bf17d6f2166d48b
Satbir Jhuti <satbirjhuti@gmail.com>
2015-10-15 10:33:27 -0400

    Assign response to $scope vars

--- js/controllers/catalog-ctrl.js
+++ js/controllers/catalog-ctrl.js
@@ -1,11 +1,15 @@
 catalogApp.controller('catalogCtrl', ['$scope', 'assetService', function($scope, assetService) {
 
+  $scope.assets = [];
+  $scope.totalHits = 0;
+
   $scope.getAssets = function(data) {
     var promise = assetService.getAssets({
       data: data
     });
     promise.then(function(response) {
-      console.log(response);
+      $scope.assets = response.Data;
+      $scope.totalHits = response.TotalHits;
     }, function(reason) {
       console.log(reason);
     });
