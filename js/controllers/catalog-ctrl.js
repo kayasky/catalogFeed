f4d79cf2e7924ef1dd294e8c37ce54bfc2a73d02
Satbir Jhuti <satbirjhuti@gmail.com>
2015-10-18 15:33:13 -0400

    Add pagination logic, cleanup re-org code

--- js/controllers/catalog-ctrl.js
+++ js/controllers/catalog-ctrl.js
@@ -1,28 +1,51 @@
 catalogApp.controller('CatalogCtrl', ['$scope', 'assetService', function($scope, assetService) {
 
   $scope.assets = [];
+  $scope.currentPage = 0;
+  $scope.totalPages = 1;
+  $scope.pageNumbers = [];
+  $scope.pageSize = 20;
   $scope.totalHits = 0;
+  $scope.pageOffset = 0;
 
-  $scope.getAssets = function(data) {
+  var initPagination = function(totalPages) {
+    var pageNumbers = [];
+    for (var i = 0; i < totalPages; i++) {
+      /* Add in the page number, offset it by 1 */
+      pageNumbers.push(i + 1);
+    }
+    return pageNumbers;
+  };
+
+  var parseAssets = function(assetData) {
+    assetData.forEach(function(asset, index) {
+      asset.Item.Images.forEach(function(image, index) {
+        if (image.Type === 1) {
+          /* Add logic to massage data to pluck out right image size for displaying thumbs */
+          asset.Item.thumbImage = image.ImageId;
+        }
+      });
+    });
+    return assetData;
+  };
+
+  var getAssets = function(data) {
     var promise = assetService.getAssets({
       data: data
     });
     promise.then(function(response) {
-      var originalAssets = response.data.Data;
-      originalAssets.forEach(function(asset, index) {
-        asset.Item.Images.forEach(function(image, index) {
-          if (image.Type === 1) {
-            /* Add logic to massage data to pluck out right image size for displaying thumbs */
-            asset.Item.thumbImage = image.ImageId;
-          }
-        });
-      });
-      $scope.assets = originalAssets;
+      $scope.assets = parseAssets(response.data.Data);
       $scope.totalHits = response.data.TotalHits;
+      $scope.totalPages = Math.ceil($scope.assets.length / $scope.pageSize);
+      $scope.pageNumbers = initPagination($scope.totalPages);
     }, function(reason) {
       console.log(reason);
     });
   };
 
-  $scope.getAssets();
+  $scope.gotoPage = function(page) {
+    $scope.pageOffset = (page - 1) * $scope.pageSize;
+  }
+
+  getAssets();
 }]);
