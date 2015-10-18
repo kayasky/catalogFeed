83f935f968b714f25376fa9901a792f962889bb5
Satbir Jhuti <satbirjhuti@gmail.com>
2015-10-18 17:39:13 -0400

    Extend directive tpl to include releaseYear and Duration

--- js/directives/thumbnail/thumbnail.js
+++ js/directives/thumbnail/thumbnail.js
@@ -1,12 +1,20 @@
 catalogApp.directive('assetThumb', function() {
 
+  var tpl = '<a href="#" class="thumbnail">';
+  tpl += '<img ng-src="{{asset.Item.thumbImage}}" alt="{{asset.Item.Title}}" class="img-rounded" />';
+  tpl += '<div class="thumb-overlay img-rounded"><h4 class="text-center">{{ asset.Item.Title }}';
+  tpl += '<small class="clearfix">{{ asset.Item.ReleaseYear }}</small>';
+  tpl += '<small class="clearfix">{{ asset.Item.RunTimeSec | readableDuration | date:\'HH:mm\' }}</small>';
+  tpl += '</h4></div>';
+  tpl += '</a>';
+
   var linkFn = function ($scope, $elem, attrs) {
 
   }
 
   return {
     restrict: 'E',
-    template: '<a href="#" class="thumbnail"><img ng-src="{{asset.Item.thumbImage}}" alt="{{asset.Item.Title}}" class="img-rounded"></a>',
+    template: tpl,
     scope: {
       asset: '='
     },
