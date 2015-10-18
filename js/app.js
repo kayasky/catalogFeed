d731dd8e1c12299c0aa747720c50d0e8a562a318
Satbir Jhuti <satbirjhuti@gmail.com>
2015-10-18 17:39:45 -0400

    Add a filter to convert seconds to readable duration

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 var catalogApp = angular.module('catalogApp', []);
+
+catalogApp.filter('readableDuration', [function() {
+    return function(seconds) {
+        return new Date(1970, 0, 1).setSeconds(seconds);
+    };
+}])
